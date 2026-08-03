050c03366092a2cef4b6ce7266bfcbf39dcec109
aasit96 <aasitgupta9696@gmail.com>
2026-08-03 15:59:18 +0530

    Time: 7 ms (32.45%), Space: 97.2 MB (60.89%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -5,12 +5,8 @@ public:
         if(n<3){
             return -1;
         }
-        int mx=INT_MIN;
-        int mn=INT_MAX;
-        for(int i=0;i<3;i++){
-             mn=min(mn,nums[i]);
-             mx=max(mx,nums[i]);
-        }
+         int mn=min({nums[0],nums[1],nums[2]});
+         int mx=max({nums[0],nums[1],nums[2]});
              for(int i=0;i<3;i++){
              if(nums[i]>mn && nums[i]<mx)
                 return nums[i];
