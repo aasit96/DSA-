4369654f86de073dc90850dad8f0a2b49821caa4
aasit96 <aasitgupta9696@gmail.com>
2026-08-12 10:47:44 +0530

    Time: 3 ms (92.06%), Space: 26 MB (81.48%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         int start=1,end=0,mid;
         for(int i=0;i<n;i++){
-            start=min(start,nums[i]);
             end=max(end,nums[i]);
         }
         int ans=-1;
