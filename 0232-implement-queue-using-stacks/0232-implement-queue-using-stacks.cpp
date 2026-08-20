a5363fcccabe66a99a24cf2fdd3d94be5c7c3b8f
aasit96 <aasitgupta9696@gmail.com>
2026-08-20 15:25:56 +0530

    Time: 0 ms (100%), Space: 10.1 MB (88.12%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,38 @@
+class MyQueue {
+private:
+    stack<int> inStack;
+    stack<int> outStack;
+
+    void transfer() {
+        if (outStack.empty()) {
+            while (!inStack.empty()) {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+    }
+
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        inStack.push(x);
+    }
+
+    int pop() {
+        transfer();
+        int val = outStack.top();
+        outStack.pop();
+        return val;
+    }
+
+    int peek() {
+        transfer();
+        return outStack.top();
+    }
+
+    bool empty() {
+        return inStack.empty() && outStack.empty();
+    }
+};
