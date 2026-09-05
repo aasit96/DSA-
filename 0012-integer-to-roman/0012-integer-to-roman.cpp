c4c4b643bb0dcef56d0d94adf32d670c28e4d2ce
aasit96 <aasitgupta9696@gmail.com>
2026-09-05 11:41:10 +0530

    Time: 0 ms (100%), Space: 9.1 MB (88.53%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+       string roman(int c){
+        if(c==1)
+        return "I";
+        else if(c==2)
+        return "II";
+        else if(c==3)
+        return "III";
+        else if(c==4)
+        return "IV";
+        else if(c==5)
+        return "V";
+        else if(c==6)
+        return "VI";
+        else if(c==7)
+        return "VII";
+        else if(c==8)
+        return "VIII";
+        else if(c==9)
+        return "IX";
+        else if(c==10)
+        return "X";
+        else if(c==20)
+        return "XX";
+        else if(c==30)
+        return "XXX";
+        else if(c==40)
+        return "XL";
+        else if(c==50)
+        return "L";
+        else if(c==60)
+        return "LX";
+        else if(c==70)
+        return "LXX";
+        else if(c==80)
+        return "LXXX";
+        else if(c==90)
+        return "XC";
+        else if(c==100)
+        return "C";
+        else if(c==200)
+        return "CC";
+        else if(c==300)
+        return "CCC";
+        else if(c==400)
+        return "CD";
+        else if(c==500)
+        return "D";
+        else if(c==600)
+        return "DC";
+        else if(c==700)
+        return "DCC";
+        else if(c==800)
+        return "DCCC";
+        else if(c==900)
+        return "CM";
+        else if(c==1000)
+        return "M";
+        else if(c==2000)
+        return "MM";
+        else if(c==3000)
+        return "MMM";
+        return "";
+       }
+    string intToRoman(int num) {
+        string ans;
+            // 1000 place nikalne ke liye
+          ans+=roman(num/1000*1000);
+          //100 place nikalne ke liye
+          ans+=roman(((num%1000)/100)*100);
+          //10 place nikalne ke liye
+          ans+=roman(((num%100)/10)*10);
+          // 1 place nikalne ke liye
+          ans+=roman(num%10);
+        
+           return ans;
+    }
+};
