e3e60a65f9b530475e739bb4e5bc43f523e2f4f1
aasit96 <aasitgupta9696@gmail.com>
2026-09-20 08:57:38 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum=0;
+        for(int i=0;i<s.size();i++){
+            sum+=(122-s[i]+1)*(i+1);
+        }
+        return sum;
+    }
+};
